5fa28d1ce68d876493b36bd4afcb23fb5ac3cab9
whiskytina <weitian.bnu@gmail.com>
2017-07-12 15:59:12 +0800

    update ava download using proxy

--- ava_download.py
+++ ava_download.py
@@ -1,4 +1,12 @@
+
 # coding: utf-8
+
+# ## Pre-requirements
+# 1. 安装[SSDB](https://github.com/jhao104/memory-notes/blob/master/SSDB/SSDB安装配置记录.md) Server并启动服务；
+# 2. 部署[proxy_pool](https://github.com/jhao104/proxy_pool)；注意将配置文件里的DB更改为SSDB，同时修改SSDB的Host和Port；启动proxy_pool
+
+# In[1]:
+
 import urllib2
 from bs4 import BeautifulSoup
 import urllib
@@ -6,63 +14,108 @@ import logging
 import time
 import requests
 import os
-import random
+import json
+import shutil
+import tqdm
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%m-%d %H:%M')
 
+def get_proxy():
+    return requests.get("http://127.0.0.1:5000/get/").content
+
+def get_all_proxy():
+    try:
+        return json.loads(requests.get("http://127.0.0.1:5000/get_all/").content)
+    except:
+        return []
+
+def delete_proxy(proxy):
+    try:
+        requests.get("http://127.0.0.1:5000/delete/?proxy={}".format(proxy))
+        logging.info("[proxy]%s is invalid and has been removed."%proxy)
+    except:
+        logging.error("[proxy]%s delete failed"%proxy)
+    
 class WebParser(object):
     def __init__(self, wait_second=1, max_retry_time=10):
+        self.url_pattern = "http://www.dpchallenge.com/image.php?IMAGE_ID=%s"
+        
         self.html_cache_path = "./data/html_cache/"
-        self.img_path = "./data/img_cache/"
+        self.img_cache_path = "./data/img_cache/"
+        if not os.path.exists(self.html_cache_path):
+            os.makedirs(self.html_cache_path)
+        if not os.path.exists(self.img_cache_path):
+            os.makedirs(self.img_cache_path)
+        
+        self.imgid = -1
         self.soup = None
+        
         self.wait_second = wait_second
         self.max_retry_time = max_retry_time
+        
+        self.MIN_HTML_SIZE = 1024
+        self.MAX_FAILED_CNT = 10
+        self.failed_proxy = {}
+        
+    def __get_html_response(self, url, valid_size=-1):
+        html = None
+        for _ in range(self.max_retry_time):
+            user_agent = "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24"
+            headers = {'User-Agent': user_agent}
+            proxy = get_proxy()
+            self.failed_proxy.setdefault(proxy, 0)
+            try:
+                html = requests.get(url=url, 
+                                    headers=headers, 
+                                    proxies={"http": "http://{}".format(proxy)})\
+                               .content
+                if len(html) < valid_size:
+                    html = None
+                    raise Exception("Invalid html response!")
+            except Exception, e:
+                self.failed_proxy[proxy] += 1
+                logging.error("[proxy]%s failed $d times"%(proxy, self.failed_proxy[proxy]))
+            else:
+                self.failed_proxy[proxy] = 0
+                break
+            finally:
+                time.sleep(self.wait_second)
+        return html
     
-    def build_request_url(self, imgid):
-        url_pattern = "http://www.dpchallenge.com/image.php?IMAGE_ID=%s"
-        return url_pattern%imgid
-    
-    def build_request_headers(self):
-        user_agent = "Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24"
-        request_headers = {'User-Agent': user_agent}
-        return request_headers
 
     def load_html(self, imgid):
-        cache_file = self.html_cache_path + "%s.html"%imgid
+        self.imgid = imgid
+        cache_file = self.html_cache_path + "%s.html"%self.imgid
         if os.path.exists(cache_file):
-            logging.info("[imgid=%s]html has been cached."%imgid)
+            logging.info("[imgid=%s]html has been cached."%self.imgid)
             html = open(cache_file, 'r').read()
         else:
-            url = self.build_request_url(imgid)
-            headers = self.build_request_headers()
-            is_opened = False
-            for _ in range(self.max_retry_time):
-                try:
-                    html = requests.get(url=url, headers=headers).content
-                except Exception, e:
-                    logging.error(str(e))
-                else:
-                    self.save_html(html, cache_file)
-                    logging.info("[imgid=%s]download html successfully."%imgid)
-                    is_opened = True
-                    break
-                finally:
-                    time.sleep(self.wait_second)
-            if not is_opened:
-                logging.warning("[imgid=%s]download html failed."%imgid)
+            url = self.url_pattern%self.imgid
+            html = self.__get_html_response(url, valid_size=self.MIN_HTML_SIZE)
+            if html is None:
+                logging.warning("[imgid=%s]download html failed."%self.imgid)
                 return False
-        self.soup = BeautifulSoup(html, 'html.parser')
+            
+            self.save_html(html, cache_file)
+            logging.info("[imgid=%s]download html successfully."%self.imgid)
+            
+        try:
+            self.soup = BeautifulSoup(html, 'html.parser')
+        except:
+            logging.error("[imgid=%s]Parse htmlSoup failed."%self.imgid)
+            return False
+        
         return True
 
-    def save_html(self, html, cache_file):
-        fhtml = open(cache_file, 'w')
-        fhtml.write(html)
-        fhtml.close()
-
-    def save_image(self, imgid):
-        cached_img = self.img_path + "%s.jpg"%imgid
+    def save_html(self, html, cache_file, mod='w'):
+        with open(cache_file, mod) as fhtml:
+            fhtml.write(html)
+        
+    def save_image(self):
+        imgid = self.imgid
+        cached_img = self.img_cache_path + "%s.jpg"%imgid
         if os.path.exists(cached_img):
             logging.info("[imgid=%s]image has been cached."%imgid)
         else:
@@ -70,13 +123,14 @@ class WebParser(object):
             if img_url is None:
                 logging.warning("[imgid=%s]image does not exist."%imgid)
                 return False
-            try:
-                urllib.urlretrieve(img_url, cached_img)
-            except Exception, e:
+            
+            img = self.__get_html_response(img_url, valid_size=self.MIN_HTML_SIZE)
+            if img is None:
                 logging.warning("[imgid=%s]image caches failed."%imgid)
                 return False
-            else:
-                logging.info("[imgid=%s]image caches successfully."%imgid)
+            
+            self.save_html(img, cached_img, mod='wb')
+            logging.info("[imgid=%s]image caches successfully."%imgid)
         return True
 
     def get_img_url(self):
@@ -85,14 +139,28 @@ class WebParser(object):
             return None
         else:
             return img_container.contents[1].get("src", None)
+        
+    def rm_failed_proxy(self):
+        for proxy, failed_cnt in self.failed_proxy.items():
+            if failed_cnt >= self.MAX_FAILED_CNT:
+                delete_proxy(proxy)
 
 
-web_parser = WebParser(wait_second=1, max_retry_time=5)
+# In[ ]:
+
+web_parser = WebParser(wait_second=0, max_retry_time=10)
 
 with open("./data/ava/AVA.txt", 'r') as fin:
-    for i, line in enumerate(fin):
+    for line in tqdm(fin):
+        while len(get_all_proxy) == 0:
+            logging.info("No avaliable proxy now")
+            time.sleep(5*60)
+        
         fields = line.strip().split(" ")
         imgid = fields[1]
+        
         if web_parser.load_html(imgid):
-            web_parser.save_image(imgid)
+            web_parser.save_image()
+            
+        web_parser.rm_failed_proxy()
 
